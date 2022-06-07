52e4f0a8bc3354f30755d857dd8163a7a0c981a8
RomanSchmid <rrr.schmid@gmail.com>
2022-06-07 23:26:28 +0200

    Added JQuery section scroll feature

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function($) {
+    $(function() {
+         /* Scroll to home */
+        $(".jq--scroll-home").click(function() {
+            $("html, body").animate({scrollTop: $(".jq--home").offset().top}, 1000);
+        });
+
+        /* Scroll to about me */
+        $(".jq--scroll-about").click(function() {
+            $("html, body").animate({scrollTop: $(".jq--about").offset().top}, 800);
+        });
+
+        /* Scroll to contact */
+        $(".jq--scroll-contact").click(function() {
+            $("html, body").animate({scrollTop: $(".jq--contact").offset().top}, 600);
+        });
+    });
+})(jQuery);
